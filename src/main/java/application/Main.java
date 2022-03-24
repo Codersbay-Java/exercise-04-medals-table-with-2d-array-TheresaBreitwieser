7733ee72b00a7607d3e8f52c7235a0e2d40e8a12
TheresaBreitwieser <theresabreitwieser@gmail.com>
2022-03-24 14:45:51 +0100

    Result

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,7 +12,37 @@ public class Main {
 		int[][] medals = {{11, 12, 6}, {9, 9, 7}, {9, 7, 7}, {8, 6, 8},
 				{7, 10, 7}, {7, 2, 5}, {6, 3, 2}, {5, 4, 5}};
 
+		int sum=0;
+
+		for(int i=0; i< countries.length; i++){
+			System.out.print("Country: " + countries[i] + ":");
+			sum=0;
+			for(int j=0; j<medals[i].length; j++) {
+				if(j<medals[i].length -1) {
+					System.out.print(medals[i][j] + ",");
+				} else {
+					System.out.print(medals[i][j]);
+				}
+				sum=sum+medals[i][j];
+			}
+			System.out.print(" --> Medals in total: " +sum);
+			System.out.println();
+		}
+
+		int[] result = getAllSilverMedals(medals);
+		System.out.println(Arrays.toString(result));
+
 	}
 
-	//test neu
+	//Task 2
+	public static int[] getAllSilverMedals(int[][] medals) {
+		int[] result= new int[medals.length];
+		int j=1;
+
+		for(int i=0; i<medals.length; i++) {
+			result[i] = medals[i][j];
+			}
+		return result;
+	}
+
 }
